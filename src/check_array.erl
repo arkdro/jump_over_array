a54c603c5e46953e6b87e56474d4a51575564c67
Arkadiy Drovosekov <arkdro@gmail.com>
2016-01-08 13:53:08 +0300

    prepare and fill storage

--- src/check_array.erl
+++ src/check_array.erl
@@ -5,5 +5,22 @@
         ]).
 
 check(L) ->
+    Initial = prepare_storage(),
+    Filled = fill_storage(Initial, L),
     erlang:error(not_implemented).
+
+%% ===================================================================
+%% Internal functions
+%% ===================================================================
+
+prepare_storage() ->
+    ets:new(?MODULE, [set]).
+
+fill_storage(Storage, L) ->
+    lists:foldl(fun store_one_item/2, {0, Storage}, L).
+
+store_one_item(Val, {Idx, Storage}) ->
+    Item = {Idx, Val},
+    ets:insert(Storage, Item),
+    {Idx + 1, Storage}.
 
