2228ef910beac69e28b5813c4108ef1b49b3803f
Arkadiy Drovosekov <arkdro@gmail.com>
2016-01-08 14:34:52 +0300

    implement check

--- src/check_array.erl
+++ src/check_array.erl
@@ -4,10 +4,13 @@
          check/1
         ]).
 
+-spec check(list(integer())) -> {ok, pos_integer()} | never.
 check(L) ->
     Initial = prepare_storage(),
     Filled = fill_storage(Initial, L),
-    erlang:error(not_implemented).
+    Idx = 0,
+    Jumps = 1,
+    check_priv(Idx, Jumps, Filled).
 
 %% ===================================================================
 %% Internal functions
@@ -20,7 +23,45 @@ fill_storage(Storage, L) ->
     lists:foldl(fun store_one_item/2, {0, Storage}, L).
 
 store_one_item(Val, {Idx, Storage}) ->
-    Item = {Idx, Val},
+    Item = {Idx, Val, free},
     ets:insert(Storage, Item),
     {Idx + 1, Storage}.
+
+fetch_one_item(Idx, Storage) ->
+    [Item] = ets:lookup(Storage, Idx),
+    Item.
+
+check_priv(Idx, Jumps, {Size, Storage}) ->
+    Updated = mark_used_index(Idx, Storage),
+    Next = get_next_idx(Idx, Updated),
+    case check_index(Next, Size, Updated) of
+        outside ->
+            {ok, Jumps};
+        loop ->
+            never;
+        inside ->
+            check_priv(Next, Jumps + 1, {Size, Updated})
+    end.
+
+mark_used_index(Idx, Storage) ->
+    {_, Val, _} = fetch_one_item(Idx, Storage),
+    Item = {Idx, Val, used},
+    ets:insert(Storage, Item),
+    Storage.
+
+get_next_idx(Idx, Storage) ->
+    {_, Val, _} = fetch_one_item(Idx, Storage),
+    Idx + Val.
+
+check_index(Idx, _, _) when Idx < 0 ->
+    outside;
+check_index(Idx, Size, _) when Idx >= Size ->
+    outside;
+check_index(Idx, _, Storage) ->
+    case fetch_one_item(Idx, Storage) of
+        {_, _, used} ->
+            loop;
+        {_, _, free} ->
+            inside
+    end.
 
